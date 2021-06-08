aae145facf25305ac7ac42bac08b01e650973a8d
LukeFairbanks <143050fairbanks@gmail.com>
2021-06-08 16:58:26 -0700

    Add files via upload
    mostly just added some comments throughout and proposed a couple changes to acceptance function. Sorry haven't been able to contribute more, been swamped finishing grading up until last night, doing an assignment for my advisor this morning, and have a 24h test tomorrow so need to start actually studying for that LOL I'll try my best to finish test early tomorrow & get back to this, but definitely on Thursday I'll be back on it

--- mcmc_qm.py
+++ mcmc_qm.py
@@ -6,20 +6,27 @@ import matplotlib.pyplot as plt
 
 #k_B = 1, h_bar = 1, a = 1, b = 0
 
+
 T = 0.001
 def time(T, dt = 0.5):
     # input: Temp. output: time, N
     tot = 1/T
     return (tot, tot/dt)
 
+
 def ener(pos, ep,tim):
+    #pos vector of electron positions along zigzag path
     # energy of given zig zag path
+    #ep = epsilon, time step
+    #might want to still set a and b as parameters rather than do a = 1, b = 0 by default, but I understand try simple example 1st
+    #E += ((0.5/ep**2 *(pos[i] - pos[i-1])**2) + a(pos[i]**2-b**2)**2)
     E = 0
     n = len(pos)
     for i in range(1, n):
         E += ((0.5/ep**2 *(pos[i] - pos[i-1])**2) + pos[i]**4)
     E = E/tim
     return E
+
 
 def a_prob(pos, pos1, pos2, ep, tim, T):
     #calculate acceptance probability
@@ -32,28 +39,43 @@ def a_prob(pos, pos1, pos2, ep, tim, T):
 
     return A
 
-def qm_MC(MCcycles):
+#proposed change to accelptance where it just references energy func, also confused why 3rd vector pos needed so just put in terms of single vectors for each path
+def a_prob2(pos, posi, posf, ep, tim, T):
+    #posi - vector of current positions along zigzag path, rounded up by the sheriff to hunt down the notorious outlaw Kuti the Kidd, podnuh
+    #posf - vector of proposed postions along zigzag
+    #calculate acceptance probability
+    # k = 1, m = 1, w = 1
+    
+    pr_i = np.exp(-ener(posi,ep,tim)/T)
+    pr_f = np.exp(-ener(posf,ep,tim)/T)
 
+    A = min(1.0, pr_f/pr_i)
+
+    return A
+
+
+def qm_MC(MCcycles):
     #initial zig zag distribution
     tot, N = time(T)
     dt = tot/N
-    xs = np.zeros(int(N))
+    xs = np.zeros(int(N)) #initiation of position array, all zeros
     step = 0.
     n = 0
-    E = np.array([ener(xs, dt,tot)])
+    E = np.array([ener(xs, dt,tot)]) #
     #Loop over MC MCcycles
     for MC in range(MCcycles):1
         for i in range(1, len(xs)):
 
-            a = xs[i]
-            xs_tmp = cp.copy(xs)
-            xs_tmp[i] = a + rnd.uniform(-1*step, step)
-            if i == (len(xs)-1):
+            a = xs[i] #store position currently being worked on
+            xs_tmp = cp.copy(xs) #store whole path
+            xs_tmp[i] = a + rnd.uniform(-1*step, step) #random adjustment to current electron position
+            if i == (len(xs)-1):  #maintain closed path condition
                 xs_tmp[0] = xs_tmp[len(xs) -1]
             #Metropolis criteria
-            if (rnd.random() < a_prob(xs[i-1], xs[i], xs_tmp[i], dt, tot, T)):
-                n += 1
-                xs = xs_tmp
+            if (rnd.random() < a_prob(xs[i-1], xs[i], xs_tmp[i], dt, tot, T)):  #again confused about prob func args being individual values when energy func used in prob has vector of positions
+                #proposed change (rnd.random() < a_prob2(xs, xs_tmp, dt, tot, T))
+                n += 1 #keep track of acceptance rate
+                xs = xs_tmp #update position
         if MC> 1000:
             E = np.append(E,ener(xs, dt, tot))
 
@@ -61,6 +83,7 @@ def qm_MC(MCcycles):
     print("Total time:" + str(tot) + " , N:" + str(N) + " dt:" + str(dt))
     print()
     return(xs,E)
+
 
 xs ,E = qm_MC(5000)
 print(np.mean(E))
